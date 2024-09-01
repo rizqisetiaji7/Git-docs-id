3e0a61d00e04a0a3a2982ac9094bfff648580f08
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2024-09-01 12:45:43 +0700

    Update link structure

--- constants/Links.mjs
+++ constants/Links.mjs
@@ -1,38 +1,32 @@
 export const SIDEBAR_LINKS = {
-	'/docs/basic/': {
-		text: 'Git Dasar',
-		items: [
-			{ text: 'Intro', link: '/docs/basic/' },
-			{ text: 'Version Control', link: '/docs/basic/version-control' },
-			{ text: 'Git', link: '/docs/basic/git' },
-			{ text: 'Konfigurasi', link: '/docs/basic/konfigurasi' },
-			{ text: 'Repository', link: '/docs/basic/repository' },
-			{ text: 'Workflow', link: '/docs/basic/workflow' },
-		]
-	},
-	'/docs/git-branching/': {
-		text: 'Git Branching',
-		items: [
-			{ text: 'Intro', link: '/docs/git-branching/' },
-		]
-	},
-	'/docs/git-remote/': {
-		text: 'Git Remote',
-		items: [
-			{ text: 'Intro', link: '/docs/git-remote/' },
-		]
-	},
+	'/docs/': [
+		{ text: 'Prolog', link: '/docs/' }
+	],
+	'/docs/basic/': [
+		{ text: 'Intro', link: '/docs/basic/' },
+		{ text: 'Version Control', link: '/docs/basic/version-control' },
+		{ text: 'Git', link: '/docs/basic/git' },
+		{ text: 'Konfigurasi', link: '/docs/basic/konfigurasi' },
+		{ text: 'Repository', link: '/docs/basic/repository' },
+		{ text: 'Workflow', link: '/docs/basic/workflow' },
+	],
+	'/docs/git-branching/': [
+		{ text: 'Intro', link: '/docs/git-branching/' },
+	],
+	'/docs/git-remote/': [
+		{ text: 'Intro', link: '/docs/git-remote/' },
+	],
 }
 
 
 export const NAV_LINKS = [
 	{ text: 'Home', link: '/' },
-	{ 
-		text: 'Dokumentasi',
+	{
+		text: 'Materi',
 		items: [
-			{ text: 'Git Dasar', link: '/docs/basic/' },
-			{ text: 'Git Branching', link: '/docs/git-branching/' },
-			{ text: 'Git Remote', link: '/docs/git-remote/' },
+			{ text: 'Git Dasar', link: '/docs/basic/'},
+			{ text: 'Git Branching', link: '/docs/git-branching/'},
+			{ text: 'Git Remote', link: '/docs/git-remote/'},
 		]
 	}
 ]
